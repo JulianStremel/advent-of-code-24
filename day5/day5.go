033b7dd1951ca14f3cea9970a9c0bc6c54d38ddc
Julian Stremel <julian.stremel@wema.de>
2024-12-05 11:57:17 +0100

    bogosort > algorithms :D

--- day5/day5.go
+++ day5/day5.go
@@ -3,11 +3,44 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"slices"
 	"strconv"
 	"strings"
+	"sync"
 )
+
+func check(checks, combinations [][]int) (sum int, faulty [][]int) {
+	// Part 1
+	// pages -> list of pages
+	var stop = false
+	for _, pages := range checks {
+		if stop {
+			stop = false
+		}
+		// page -> single page (int)
+		for b, page := range pages {
+			if stop {
+				break
+			}
+			// pair -> (int,int)
+			for _, pair := range combinations {
+				if page == pair[0] {
+					if slices.Contains(pages[0:b], pair[1]) {
+						stop = true
+						faulty = append(faulty, pages)
+						break
+					}
+				}
+			}
+		}
+		if !stop {
+			sum += pages[(len(pages) / 2)]
+		}
+	}
+	return sum, faulty
+}
 
 func main() {
 	var combinations [][]int
@@ -57,33 +90,28 @@ func main() {
 	}
 	readFile.Close()
 
-	// Part 1
-	// pages -> list of pages
-	var stop = false
-	var sum = 0
-	for _, pages := range checks {
-		if stop {
-			stop = false
-		}
-		// page -> single page (int)
-		for b, page := range pages {
-			if stop {
-				break
-			}
-			// pair -> (int,int)
-			for _, pair := range combinations {
-				if page == pair[0] {
-					if slices.Contains(pages[0:b], pair[1]) {
-						stop = true
-						fmt.Printf("%d does include %d\n", pages[0:b], pair[1])
-						break
-					}
-				}
-			}
-		}
-		if !stop {
-			sum += pages[(len(pages) / 2)]
-		}
-	}
+	sum, faulty := check(checks, combinations)
+
 	fmt.Printf("Part 1: %d", sum)
+	var wg sync.WaitGroup
+	for _, fault := range faulty {
+		wg.Add(1)
+		fmt.Println("started on")
+		fmt.Println(fault)
+		go func() {
+			var flt = [][]int{[]int{10}}
+			for {
+				if len(flt) == 0 {
+					break
+				}
+				//fmt.Println(flt)
+				rand.Shuffle(len(fault), func(i, j int) { fault[i], fault[j] = fault[j], fault[i] })
+				_, flt = check([][]int{fault}, combinations)
+			}
+			fmt.Println(fault)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
 }
