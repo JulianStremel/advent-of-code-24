859b77d8bf9e2f8a559fa30d31f47c4a1a27b72d
Julian Stremel <julian.stremel@wema.de>
2024-12-06 14:03:24 +0100

    Day 6 Part 2 solved

--- day6/day6.go
+++ day6/day6.go
@@ -4,95 +4,96 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"slices"
 	"strings"
 )
 
 func moveUp(grid [][]string, start []int) (movable bool, direction int, steps int, grd [][]string) {
 	if start[0] <= 0 {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return false, 0, 0, grid
 	}
 	if grid[start[0]-1][start[1]] != "#" {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return true, 0, 1, grid
 	} else {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return true, 1, 0, grid
 	}
 }
 
 func moveRight(grid [][]string, start []int) (movable bool, direction int, steps int, grd [][]string) {
 	if start[1] >= len(grid[0])-1 {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return false, 1, 0, grid
 	}
 	if grid[start[0]][start[1]+1] != "#" {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return true, 1, 1, grid
 	} else {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return true, 2, 0, grid
 	}
 }
 
 func moveDown(grid [][]string, start []int) (movable bool, direction int, steps int, grd [][]string) {
 	if start[0] >= len(grid)-1 {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return false, 2, 0, grid
 	}
 	if grid[start[0]+1][start[1]] != "#" {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return true, 2, 1, grid
 	} else {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return true, 3, 0, grid
 	}
 }
 
 func moveLeft(grid [][]string, start []int) (movable bool, direction int, steps int, grd [][]string) {
 	if start[1] <= 0 {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return false, 3, 0, grid
 	}
 	if grid[start[0]][start[1]-1] != "#" {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return true, 3, 1, grid
 	} else {
-		//grid[start[0]][start[1]] = "X"
+		grid[start[0]][start[1]] = "X"
 		return true, 0, 0, grid
 	}
 }
 
-func move(grid [][]string, start []int, direction int) (steps int) {
-
+func move(grid [][]string, start []int, direction int) (steps int, path [][]int) {
 	var mov bool = true
 	var stp int
+	var dir = direction
+	var strt = start
+	var grd = grid
 	steps = 0
 	for mov {
-		switch direction {
+		switch dir {
 		case 0:
-			mov, direction, stp, grid = moveUp(grid, start)
+			mov, dir, stp, grd = moveUp(grd, strt)
 			if mov && stp > 0 {
-				start[0] = start[0] - 1
+				strt[0] = strt[0] - 1
 			}
 			steps += stp
 		case 1:
-			mov, direction, stp, grid = moveRight(grid, start)
+			mov, dir, stp, grd = moveRight(grd, strt)
 			if mov && stp > 0 {
-				start[1] = start[1] + 1
+				strt[1] = strt[1] + 1
 			}
 			steps += stp
 		case 2:
-			mov, direction, stp, grid = moveDown(grid, start)
+			mov, dir, stp, grd = moveDown(grd, strt)
 			if mov && stp > 0 {
-				start[0] = start[0] + 1
+				strt[0] = strt[0] + 1
 			}
 			steps += stp
 		case 3:
-			mov, direction, stp, grid = moveLeft(grid, start)
+			mov, dir, stp, grd = moveLeft(grd, strt)
 			if mov && stp > 0 {
-				start[1] = start[1] - 1
+				strt[1] = strt[1] - 1
 			}
 			steps += stp
 		default:
@@ -100,85 +101,142 @@ func move(grid [][]string, start []int, direction int) (steps int) {
 		}
 	}
 	var cnt = 0
-	for _, row := range grid {
-		for _, col := range row {
+	var ret [][]int
+	for a, row := range grd {
+		for b, col := range row {
 			if col == "X" {
 				cnt++
+				ret = append(ret, []int{a, b})
 			}
 		}
 	}
-	return cnt
+	return cnt, ret
 }
 
-func checkLoop(grid [][]string, start []int, direction int) bool {
-
-	var mov bool = true
-	var stp int
-	type dir struct {
-		Y   int
-		X   int
-		Dir int
+func moveUp2(grid [][]string, start []int) (movable bool, direction int, step int) {
+	if start[0] <= 0 {
+		return false, 0, 0
 	}
-	var directions []dir //[[Y,X,Dir][Y,X,Dir]] // if location gets passed in same direction twice = loop
+	if grid[start[0]-1][start[1]] != "#" {
+		return true, 0, 1
+	} else {
+		return true, 1, 0
+	}
+}
 
+func moveRight2(grid [][]string, start []int) (movable bool, direction int, step int) {
+	if start[1] >= len(grid[0])-1 {
+		return false, 1, 0
+	}
+	if grid[start[0]][start[1]+1] != "#" {
+		return true, 1, 1
+	} else {
+		return true, 2, 0
+	}
+}
+
+func moveDown2(grid [][]string, start []int) (movable bool, direction int, step int) {
+	if start[0] >= len(grid)-1 {
+		return false, 2, 0
+	}
+	if grid[start[0]+1][start[1]] != "#" {
+		return true, 2, 1
+	} else {
+		return true, 3, 0
+	}
+}
+
+func moveLeft2(grid [][]string, start []int) (movable bool, direction int, step int) {
+	if start[1] <= 0 {
+		return false, 3, 0
+	}
+	if grid[start[0]][start[1]-1] != "#" {
+		return true, 3, 1
+	} else {
+		return true, 0, 0
+	}
+}
+
+type dir struct {
+	y   int
+	x   int
+	dir int
+}
+
+func (d dir) compare(c dir) bool {
+	if c.x == d.x && c.y == d.y && c.dir == d.dir {
+		return true
+	}
+	return false
+}
+
+func (d dir) isCointained(c []dir) bool {
+	for _, di := range c {
+		if d.compare(di) {
+			return true
+		}
+	}
+	return false
+}
+
+func move2(grid [][]string, start []int, direction int) bool {
+	var mov bool = true
+	var dirs []dir
 	var tmp dir
+	var stp int
 	for mov {
 		switch direction {
 		case 0:
-			mov, direction, stp, grid = moveUp(grid, start)
+			mov, direction, stp = moveUp2(grid, start)
 			if mov && stp > 0 {
 				tmp = dir{start[0], start[1], direction}
-				if slices.Contains(directions, tmp) {
+				if tmp.isCointained(dirs) {
 					return true
 				}
-				directions = append(directions, tmp)
 				start[0] = start[0] - 1
+				dirs = append(dirs, tmp)
 			}
-
 		case 1:
-			mov, direction, stp, grid = moveRight(grid, start)
+			mov, direction, stp = moveRight2(grid, start)
 			if mov && stp > 0 {
 				tmp = dir{start[0], start[1], direction}
-				if slices.Contains(directions, tmp) {
+				if tmp.isCointained(dirs) {
 					return true
 				}
-				directions = append(directions, tmp)
 				start[1] = start[1] + 1
+				dirs = append(dirs, tmp)
 			}
-
 		case 2:
-			mov, direction, stp, grid = moveDown(grid, start)
+			mov, direction, stp = moveDown2(grid, start)
 			if mov && stp > 0 {
 				tmp = dir{start[0], start[1], direction}
-				if slices.Contains(directions, tmp) {
+				if tmp.isCointained(dirs) {
 					return true
 				}
-				directions = append(directions, tmp)
 				start[0] = start[0] + 1
+				dirs = append(dirs, tmp)
 			}
-
 		case 3:
-			mov, direction, stp, grid = moveLeft(grid, start)
+			mov, direction, stp = moveLeft2(grid, start)
 			if mov && stp > 0 {
 				tmp = dir{start[0], start[1], direction}
-				if slices.Contains(directions, tmp) {
+				if tmp.isCointained(dirs) {
 					return true
 				}
-				directions = append(directions, tmp)
 				start[1] = start[1] - 1
+				dirs = append(dirs, tmp)
 			}
-
 		default:
 			fmt.Println("no way")
 		}
 	}
 	return false
-
 }
 
 func main() {
 	var fileLines [][]string
 	var fileLines2 [][]string
+
 	readFile, err := os.Open("day6/input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -187,6 +245,7 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	var start []int
+	var start2 []int
 	var direction int // 0 -> up | 1 -> right | 2 -> down | 3 -> left
 	var line []string
 	var line2 []string
@@ -215,26 +274,19 @@ func main() {
 		a++
 	}
 	readFile.Close()
-
-	fmt.Println(move(fileLines, start, direction))
+	start2 = append(start2, start...)
+	stp, path := move(fileLines, start, direction)
 
 	var cnt = 0
-	for a, line := range fileLines2 {
-		for b, row := range line {
-			if row != "." {
-				continue
-			}
-			fileLines2[a][b] = "#"
-			if checkLoop(fileLines2, start, direction) {
-				cnt++
-				fmt.Println(cnt)
-			}
-			fileLines2[a][b] = ":"
-			fmt.Println(a, b)
+	for a, coord := range path {
+		fileLines2[coord[0]][coord[1]] = "#"
+		if move2(fileLines2, []int{start2[0], start2[1]}, 0) {
+			cnt++
 		}
+		fileLines2[coord[0]][coord[1]] = "."
+		fmt.Printf("tested %d of %d possbile positions (%f%%)\n", a, len(path), float32(a)/float32(len(path))*100.0)
 	}
 
-	fmt.Println(fileLines2)
-	fmt.Println(cnt)
-
+	fmt.Printf("Part 1: %d", stp)
+	fmt.Printf("Part 2: %d", cnt)
 }
