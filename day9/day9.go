ae1e396f2b5a205e61e309033f7d92bc06ab47c7
Julian Stremel <julian.stremel@wema.de>
2024-12-09 15:47:10 +0100

    part 2 still not working

--- day9/day9.go
+++ day9/day9.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 )
 
@@ -11,6 +12,14 @@ type Storage struct {
 	input         []int
 	decrompressed []int
 	checksum      int
+	chunks_free   []Chunk
+	chunks_file   []Chunk
+}
+
+type Chunk struct {
+	id    int
+	index int
+	size  int
 }
 
 func (s *Storage) read(path string) {
@@ -33,16 +42,21 @@ func (s *Storage) read(path string) {
 
 func (s *Storage) decompress() {
 	var id = 0
+	var virtual_index = 0
 	for index, num := range s.input {
 		if index%2 == 0 {
 			for range num {
 				s.decrompressed = append(s.decrompressed, id)
 			}
+			s.chunks_file = append(s.chunks_file, Chunk{id, virtual_index, num})
+			virtual_index += num
 			id++
 		} else {
 			for range num {
 				s.decrompressed = append(s.decrompressed, -1)
 			}
+			s.chunks_free = append(s.chunks_free, Chunk{-1, virtual_index, num})
+			virtual_index += num
 		}
 	}
 }
@@ -67,6 +81,25 @@ func (s *Storage) compact() {
 	s.decrompressed = s.decrompressed[:tail+1]
 }
 
+func (s *Storage) compactNonFragment() {
+	for a, _ := range s.chunks_file {
+		chunk_file := s.chunks_file[len(s.chunks_file)-1-a]
+		for b, chunk_free := range s.chunks_free {
+			if chunk_file.size <= chunk_free.size {
+				// switch file chunk into free space
+				for c := range chunk_file.size {
+					s.decrompressed[chunk_free.index+c] = s.decrompressed[chunk_file.index+c]
+					s.decrompressed[chunk_file.index+c] = -1
+				}
+				if chunk_file.size < chunk_free.size {
+					s.chunks_free = append(s.chunks_free, Chunk{chunk_free.index + chunk_file.size, chunk_free.size - chunk_file.size, -1})
+				}
+				slices.Delete(s.chunks_free, b, b+1)
+			}
+		}
+	}
+}
+
 func (s *Storage) updateChecksum() {
 	for a, num := range s.decrompressed {
 		if num > 0 {
@@ -75,13 +108,13 @@ func (s *Storage) updateChecksum() {
 	}
 }
 
-// 6519155394575 too high
-
 func main() {
 	storage := Storage{}
-	storage.read("day9/input.txt")
+	storage.read("day9/input_test.txt")
 	storage.decompress()
-	storage.compact()
+	fmt.Println(storage.decrompressed)
+	storage.compactNonFragment()
 	storage.updateChecksum()
+	fmt.Println(storage.decrompressed)
 	fmt.Println(storage.checksum)
 }
