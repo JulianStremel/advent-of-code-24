acebbb17984022b29ce79e984a6919f08649d079
Julian Stremel <julian.stremel@wema.de>
2024-12-11 17:51:43 +0100

    cleanup

--- day11/day11.go
+++ day11/day11.go
@@ -7,7 +7,6 @@ import (
 	"slices"
 	"strconv"
 	"strings"
-	"time"
 )
 
 type Pluto struct {
@@ -120,22 +119,22 @@ func (p *Pluto) blinkRecurs(depth, num int) (result int) {
 }
 
 func main() {
-	start := time.Now()
 	pluto := Pluto{}
 	pluto.load("day11/input.txt")
+	//start := time.Now()
 	for range 25 {
 		pluto.blink()
 	}
-	duration := time.Since(start)
-	fmt.Println("Recurse 1 took: ", duration.Microseconds(), " us")
-	fmt.Println("Part 1:", pluto.sum)
+	//duration := time.Since(start)
+	//fmt.Println("Recurse 1 took: ", duration.Milliseconds(), " ms")
+	fmt.Println("Part 1:", len(pluto.pebbles))
 
-	start = time.Now()
 	pluto = Pluto{}
 	pluto.load("day11/input.txt")
+	//start = time.Now()
 	pluto.blink2(75)
-	duration = time.Since(start)
-	fmt.Println("Recurse 1 took: ", duration.Microseconds(), " us")
+	//duration = time.Since(start)
+	//fmt.Println("Recurse 1 took: ", duration.Milliseconds(), " ms")
 	fmt.Println("Part 2:", pluto.sum)
 
 }
