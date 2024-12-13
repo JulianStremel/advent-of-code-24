ff0f75d58c8dc56cacfc1837c16ebc7291e54632
Jul Str <74060086+JulianStremel@users.noreply.github.com>
2024-12-13 01:46:04 +0100

    done for the day but not with part 2 ...

--- day12/day12.go
+++ day12/day12.go
@@ -27,6 +27,20 @@ type Point struct {
 	x int
 }
 
+func uniquePoints(points []Point) []Point {
+	uniqueMap := make(map[Point]struct{})
+	uniqueList := []Point{}
+
+	for _, point := range points {
+		if _, exists := uniqueMap[point]; !exists {
+			uniqueMap[point] = struct{}{}
+			uniqueList = append(uniqueList, point)
+		}
+	}
+
+	return uniqueList
+}
+
 func (g *Garden) load(path string) {
 	readFile, err := os.Open(path)
 	if err != nil {
@@ -120,6 +134,104 @@ func (g *Garden) recursiveSearch(parent Point, flower string) (perimeter int) {
 	return perimeter
 }
 
+func (g *Garden) calculateBulk() {
+
+	// Idea:
+	// first walk horizontally then walk vertically
+	// keep in mind that :
+	// X X X X X X
+	// X A A A A X
+	// X X X X X X
+	// A in this case would be seen as 4 Walls
+
+	ret := 0
+	for _, plt := range g.plots {
+		// half a coord shiftet to the left and top to store edge positions
+		edgeGrid := []Point{}
+		for _, tile := range plt.tiles {
+			if !slices.Contains(plt.tiles, Point{y: tile.y, x: tile.x - 1}) {
+				edgeGrid = append(edgeGrid, Point{y: tile.y, x: tile.x - 1})
+			}
+			if !slices.Contains(plt.tiles, Point{y: tile.y, x: tile.x + 1}) {
+				edgeGrid = append(edgeGrid, Point{y: tile.y, x: tile.x + 1})
+			}
+			if !slices.Contains(plt.tiles, Point{y: tile.y - 1, x: tile.x}) {
+				edgeGrid = append(edgeGrid, Point{y: tile.y - 1, x: tile.x})
+			}
+			if !slices.Contains(plt.tiles, Point{y: tile.y + 1, x: tile.x}) {
+				edgeGrid = append(edgeGrid, Point{y: tile.y + 1, x: tile.x})
+			}
+		}
+		edgeGrid = uniquePoints(edgeGrid)
+		sum := 0
+		fmt.Println(plt.flower, len(plt.tiles))
+		for range edgeGrid {
+			if len(edgeGrid) == 0 {
+				break
+			}
+			edge := edgeGrid[0]
+			fmt.Println(plt.flower, edge, edgeGrid, sum)
+			if slices.Contains(edgeGrid, Point{edge.y, edge.x + 1}) {
+				cnt := 0
+				for slices.Contains(edgeGrid, Point{edge.y, edge.x + cnt}) {
+					index := slices.Index(edgeGrid, Point{edge.y, edge.x + cnt})
+					edgeGrid = slices.Delete(edgeGrid, index, index+1)
+					cnt++
+				}
+				if slices.Contains(edgeGrid, Point{edge.y + 1, edge.x + cnt - 1}) || slices.Contains(edgeGrid, Point{edge.y - 1, edge.x + cnt - 1}) {
+					edgeGrid = append(edgeGrid, Point{edge.y, edge.x + cnt - 1})
+				}
+				sum++
+				continue
+			}
+			if slices.Contains(edgeGrid, Point{edge.y, edge.x - 1}) {
+				cnt := 0
+				for slices.Contains(edgeGrid, Point{edge.y, edge.x - cnt}) {
+					index := slices.Index(edgeGrid, Point{edge.y, edge.x - cnt})
+					edgeGrid = slices.Delete(edgeGrid, index, index+1)
+					cnt++
+				}
+				if slices.Contains(edgeGrid, Point{edge.y + 1, edge.x - cnt + 1}) || slices.Contains(edgeGrid, Point{edge.y - 1, edge.x - cnt + 1}) {
+					edgeGrid = append(edgeGrid, Point{edge.y, edge.x + cnt + 1})
+				}
+				sum++
+				continue
+			}
+			if slices.Contains(edgeGrid, Point{edge.y + 1, edge.x}) {
+				cnt := 0
+				for slices.Contains(edgeGrid, Point{edge.y + cnt, edge.x}) {
+					index := slices.Index(edgeGrid, Point{edge.y + cnt, edge.x})
+					edgeGrid = slices.Delete(edgeGrid, index, index+1)
+					cnt++
+				}
+				if slices.Contains(edgeGrid, Point{edge.y + cnt - 1, edge.x + 1}) || slices.Contains(edgeGrid, Point{edge.y + cnt - 1, edge.x - 1}) {
+					edgeGrid = append(edgeGrid, Point{edge.y, edge.x + cnt - 1})
+				}
+				sum++
+				continue
+			}
+			if slices.Contains(edgeGrid, Point{edge.y - 1, edge.x}) {
+				cnt := 0
+				for slices.Contains(edgeGrid, Point{edge.y - cnt, edge.x}) {
+					index := slices.Index(edgeGrid, Point{edge.y - cnt, edge.x})
+					edgeGrid = slices.Delete(edgeGrid, index, index+1)
+					cnt++
+				}
+				if slices.Contains(edgeGrid, Point{edge.y - cnt + 1, edge.x + 1}) || slices.Contains(edgeGrid, Point{edge.y - cnt + 1, edge.x - 1}) {
+					edgeGrid = append(edgeGrid, Point{edge.y, edge.x + cnt - 1})
+				}
+				sum++
+				continue
+			}
+			sum++
+			edgeGrid = slices.Delete(edgeGrid, 0, 1)
+		}
+		fmt.Println(sum)
+		ret += sum * len(plt.tiles)
+	}
+	fmt.Println(ret)
+}
+
 func (g *Garden) findPlots() {
 	g.plots = []Plot{}
 	sum := 0
@@ -133,7 +245,6 @@ func (g *Garden) findPlots() {
 			tmp := g.recursiveSearch(tile, flower)
 			plt := Plot{flower: flower, perimeter: tmp, tiles: g.visited}
 			g.plots = append(g.plots, plt)
-			fmt.Println("start", tile, flower, len(g.visited), tmp)
 			sum += len(g.visited) * tmp
 			for _, a := range g.visited {
 				ind := slices.Index(g.tiles[flower], a)
@@ -141,11 +252,14 @@ func (g *Garden) findPlots() {
 			}
 		}
 	}
-	fmt.Println(sum)
 }
+
+// 752035 too low
+// 574712
 
 func main() {
 	garden := Garden{}
 	garden.load("day12/input.txt")
 	garden.findPlots()
+	garden.calculateBulk()
 }
